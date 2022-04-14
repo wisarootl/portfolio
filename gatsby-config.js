97ae5f3b8bcc4ee49a07955d7fdf9b9b9192d014
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2022-04-14 17:46:45 +0700

    add site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Omega Gatsby`,
+    siteUrl: `https://wisaroot.herokuapp.com/`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -9,5 +10,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
-  ],
-};
+    `gatsby-plugin-sitemap`
+  ]
+}
