1052f14f939db753fb2ea0f5c5c8236927062f0a
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2022-04-14 18:33:26 +0700

    debug sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,58 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/'
+      }
+    },
+    `gatsby-transformer-gitinfo`,
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        query: `{
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allFile(filter: {sourceInstanceName: {eq: "pages"}}) {
+            edges {
+              node {
+                fields {
+                  gitLogLatestDate
+                }
+                name
+              }
+            }
+          }
+        }`,
+        resolvePages: ({ allSitePage: { nodes: sitePages }, allFile: { edges: pageFiles } }) => {
+          return sitePages.map((page) => {
+            const pageFile = pageFiles.find(({ node }) => {
+              const fileName = node.name === 'index' ? '/' : `/${node.name}/`
+              return page.path === fileName
+            })
+
+            return { ...page, ...pageFile?.node?.fields }
+          })
+        },
+        serialize: ({ path, gitLogLatestDate }) => {
+          return {
+            url: path,
+            lastmod: gitLogLatestDate
+          }
+        },
+        createLinkInHead: true
+      }
+    }
   ]
 }
