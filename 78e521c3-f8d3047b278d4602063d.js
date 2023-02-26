fb012f8a7f71bf3ebdd8bce68949d5fdc9e42992
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2023-02-26 19:34:51 +0700

    Updates

---
+++ 78e521c3-f8d3047b278d4602063d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{v7au:function(t,a,h){"use strict";h.d(a,"a",(function(){return v}));var n=h("Lnxd");function v(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M8 5h3v9H8v3H6v-3H3V5h3V2h2v3zm10 5h3v9h-3v3h-2v-3h-3v-9h3V7h2v3z"}}]}]})(t)}}}]);
+//# sourceMappingURL=78e521c3-f8d3047b278d4602063d.js.map
